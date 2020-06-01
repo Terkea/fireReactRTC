ff0b2afe195a9abff34e0ade26feacb1e118a93d
Marian Terchila <terkeabt@gmail.com>
2020-06-01 16:13:39 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDpW_Ry3IdXCs4_5OUw4yOuh1Ar995QjMM",
-    authDomain: "fir-rtc-2fb16.firebaseapp.com",
-    databaseURL: "https://fir-rtc-2fb16.firebaseio.com",
-    projectId: "fir-rtc-2fb16",
-    storageBucket: "fir-rtc-2fb16.appspot.com",
-    messagingSenderId: "233503926980",
-    appId: "1:233503926980:web:bb546a4b4a17626c46b791"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
